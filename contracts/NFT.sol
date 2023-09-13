3b593da12e562dd6515b093f38122449e2d624eb
Leon Lemos <leonlemos26@gmail.com>
2023-09-13 13:50:00 +0100

    Withdraw Ether

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -16,6 +16,7 @@ contract NFT is ERC721Enumerable, Ownable {
     
 
     event Mint(uint256 amount, address minter);
+    event Withdraw(uint256 amount, address owner);
 
     constructor (string memory _name, string memory _symbol, uint256 _cost, uint256 _maxSupply, uint256 _allowMintingOn, string memory _baseURI) ERC721 (_name , _symbol){
         cost = _cost;
@@ -70,7 +71,21 @@ contract NFT is ERC721Enumerable, Ownable {
             tokenIds[i]=tokenOfOwnerByIndex(_owner,i);
         }
         return tokenIds;
+    }
 
+    
+    //Owner Functions
+    function withdraw() public onlyOwner{
+        uint256 balance = address(this).balance;
+
+        (bool success,) = payable(msg.sender).call{value:balance}("");
+        require (success);
+
+        emit Withdraw (balance,msg.sender);
+    }
+
+    function setCost(uint256 _newCost) public onlyOwner{
+    cost = _newCost;
     }
 
 }
